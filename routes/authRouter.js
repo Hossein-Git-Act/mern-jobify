e9621c7925512804488158e1a2eab6117907e24e
Hossein Amouzad <hossiamz84@gmail.com>
2025-08-02 11:52:58 +0330

    first commit

---
+++ routes/authRouter.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+const router = Router();
+import { login, logout, register } from '../controllers/authController.js';
+import {
+  validateRegisterInput,
+  validateLoginInput,
+} from '../middleware/validationMiddleware.js';
+
+import rateLimit from 'express-rate-limit';
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max:1,
+  message:{msg:'API rate limit exceeded , retry in 15 minutes '}
+})
+
+router.post('/register', apiLimiter, validateRegisterInput, register);
+router.post('/login', apiLimiter, validateLoginInput, login);
+router.get('/logout', logout);
+
+export default router;
