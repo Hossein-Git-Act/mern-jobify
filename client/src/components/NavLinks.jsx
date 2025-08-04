d2581b520d01e6149a60979ba31f7d0faf8e9a2b
Hossein Amouzad <hossiamz84@gmail.com>
2025-08-04 21:18:34 +0330

    finish 3 commit

--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -9,7 +9,7 @@ const NavLinks = ({ isBigSidebar }) => {
       {links.map((link) => {
         const { text, path, icon } = link;
         const { role } = user;
-        if (path === 'admin' && role !== 'admin') return;
+
         return (
           <NavLink
             to={path}
